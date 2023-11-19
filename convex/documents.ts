6aec6ccc73b4b406681bbaf2b138fc7d5b9f28c8
DAYAN <binaryones.gQ@gmail.com>
2023-11-19 23:35:17 +0600

    build: add new table for task in convex

---
+++ convex/documents.ts
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+
+import { mutation, query } from "./_generated/server";
+import { Doc, Id } from "./_generated/dataModel";
+
+export const create = mutation({
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id("documents"))
+  },
+  handler: async ( ctx, args ) => {
+    const indentity = await ctx.auth.getUserIdentity();
+
+    if (!indentity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = indentity.subject;
+    const documents = await ctx.db.insert("documents", {
+      title: args.title,
+      parentDocument: args.parentDocument,
+      userId,
+      isArchived: false,
+      isPublished: false,
+    });
+
+    return documents;
+  }
+})
